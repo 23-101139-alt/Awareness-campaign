6ede045c35b25e21dee4bc0e0f679519eb712167
23-101139-alt <23-101139@students.eui.edu.eg>
2025-12-15 23:19:58 +0200

    sec1-text-box-homepage

--- home.js
+++ home.js
@@ -9,12 +9,13 @@ document.getElementById("eng-header").innerHTML = `EN`;
 
 document.getElementById("arb-header").innerHTML = `AR`;
 
+// headerend
+document.getElementById("small-text-sec1").innerHTML = `Save Water Today for a Thirsty Tomorrow.`;
+document.getElementById("big-text-sec1").innerHTML = `Every Drop Counts!`;
 
+document.getElementById("para-sec1").innerHTML = `Water waste is becoming a serious issue, and many people don’t realize how quickly small daily habits can drain our limited resources. Every drop we save helps protect our future.`;
 
-
-
-
-
+document.getElementById("para2-sec1").innerHTML = `This campaign encourages everyone to use water wisely and make simple changes that truly matter. Together, we can reduce waste and keep our water supply safe for the next generations.`;
 
 
 
