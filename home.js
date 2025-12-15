4da3e3550c61ed64d7a45d658a464f13a5ff21f3
23-101139-alt <23-101139@students.eui.edu.eg>
2025-12-15 22:52:30 +0200

    header-homepage

---
+++ home.js
@@ -0,0 +1,39 @@
+document.getElementById("list1-header").innerHTML = `Home`;
+document.getElementById("list2-header").innerHTML = `Statistics`;
+document.getElementById("list3-header").innerHTML = `Contact`;
+document.getElementById("list4-header").innerHTML = `Gallery`;
+document.getElementById("list5-header").innerHTML = `Messages`;
+
+
+document.getElementById("eng-header").innerHTML = `EN`;
+
+document.getElementById("arb-header").innerHTML = `AR`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
