d1060bf36af91e01ba00e188ec4be84d6bd15acb
23-101139-alt <23-101139@students.eui.edu.eg>
2025-12-15 20:55:08 +0200

    text-box-login-page

---
+++ login.js
@@ -0,0 +1,6 @@
+document.getElementById("header-login").innerHTML = `Log In to Your Account`;
+    document.getElementById("placeholder-log-in-page").innerHTML = `Email Address:`;
+document.getElementById("placeholder2-log-in-page").innerHTML = `Password:`;
+document.getElementById("forgot").innerHTML = `Forgot password`;
+    document.getElementById("remember-textt").innerHTML = `Remember me`;
+document.getElementById("button-text-1").innerHTML = `Log in`;
