00c05fb621b75f35e0c289d9443a34affb371bde
23-101139-alt <23-101139@students.eui.edu.eg>
2025-12-17 22:21:15 +0200

    sec1-animation

--- home.js
+++ home.js
@@ -29,6 +29,17 @@ window.addEventListener("load", () => {
 
 
 
+// function animateOnScroll(selector) {
+//     const observer = new IntersectionObserver(entries => {
+//         entries.forEach(entry => {
+//             entry.target.classList.toggle('animate', entry.isIntersecting);
+//         });
+//     });
+
+//     document.querySelectorAll(selector).forEach(el => observer.observe(el));
+// }
+
+// animateOnScroll('.big-img-sec1');
 
 
 
@@ -36,6 +47,31 @@ window.addEventListener("load", () => {
 
 
 
+window.addEventListener("load", () => {
+
+  setTimeout(() => {
+
+
+    document.querySelector(".preloaderrr").style.display = "none";
+
+    const observer = new IntersectionObserver((entries, obs) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("animate");
+          obs.unobserve(entry.target); // play once
+        }
+      });
+    }, {
+      threshold: 0.3
+    });
+
+    document.querySelectorAll(
+      ".big-img-sec1, .img-decoration1-sec1, .img-decoration2-sec1, .img-decoration3-sec1, .img-decoration4-sec1, .img-decoration5-sec1, .img-decoration6-sec1, .img-decoration7-sec1, .img-decoration8-sec1"
+    ).forEach(el => observer.observe(el));
+
+  }, 3000); 
+
+});
 
 
 
