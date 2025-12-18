119d7d6c6c545c8e1f6c1c8d50926ac595ad0d8d
23-101139-alt <23-101139@students.eui.edu.eg>
2025-12-18 22:11:52 +0200

    messages-page

---
+++ messages.js
@@ -0,0 +1,61 @@
+document.getElementById("list1-header").innerHTML = `Home`;
+document.getElementById("list2-header").innerHTML = `Statistics`;
+document.getElementById("list3-header").innerHTML = `Contact`;
+document.getElementById("list4-header").innerHTML = `Gallery`;
+document.getElementById("list5-header").innerHTML = `Messages`;
+
+
+document.getElementById("eng-header").innerHTML = `Eng`;
+document.getElementById("login-header").innerHTML = `Log in`;
+
+// headerend
+
+// prelogin
+
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    document.querySelector(".preloaderrr").style.display = "none";
+  }, 200);
+});
+
+
+
+
+// sec1
+document.getElementById("header-sec1").innerHTML = `Contact Messages`;
+
+
+    const searchBar = document.getElementById('searchBar');
+    const searchInput = document.querySelector('.search-input');
+
+    searchBar.addEventListener('click', () => {
+    searchBar.classList.toggle('activee');
+    searchInput.focus(); 
+    });
+
+
+
+
+
+
+
+// footer
+
+document.getElementById("para-footer").innerHTML = `Empowering 
+communities worldwide to protect and preserve our most precious
+ resource.`;
+
+ document.getElementById("title1-footer").innerHTML = `Explore`;
+document.getElementById("list1-footer").innerHTML = `Home page`;
+document.getElementById("list2-footer").innerHTML = `Gallery`;
+document.getElementById("list3-footer").innerHTML = `Messages`;
+document.getElementById("list4-footer").innerHTML = `Blogs`;
+document.getElementById("list5-footer").innerHTML = `News`;
+
+ document.getElementById("title2-footer").innerHTML = `Legal`;
+document.getElementById("list6-footer").innerHTML = `Terms & Conditions`;
+document.getElementById("list7-footer").innerHTML = `Cookies`;
+document.getElementById("list8-footer").innerHTML = `Privacy Policy`;
+
+document.getElementById("last-text-footer").innerHTML = `@2025,DropSaver,AllRightsReservedbyDropSaver`;
